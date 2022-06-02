6a46762ec050cd9d03998d2b7889b3e2e2aa0ba2
haoyong4039 <1241907185@qq.com>
2022-06-02 15:35:41 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication
 {
     public static void main(String[] args)
     {
-        System.out.println("hotfix finished");
+        System.out.println("this is remote develop");
         SpringApplication.run(DemoApplication.class, args);
     }
 }
